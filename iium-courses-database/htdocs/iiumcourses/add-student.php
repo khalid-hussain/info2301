f278d57066c63a059eb7a6866c393cc286a3a408
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2015-12-07 23:46:07 +0800

    Cleaned index.php and add-student.php and added comments

--- iium-courses-database/htdocs/iiumcourses/add-student.php
+++ iium-courses-database/htdocs/iiumcourses/add-student.php
@@ -1,21 +1,30 @@
 <?php
-  $theMessage = null;
+// This is initialized as null in case there is an error
+$theMessage = null;
+
+// If-Else block when user clicks the submit button
+if (isset($_POST['submit'])){  
+  // The following is required for DB connection
+  // Specifically the $conn variable
+  require ('connect-db.php');
   
-  if (isset($_POST['submit'])){  
-    require ('connect-db.php');
-    
-    $stud_matric = $_POST['stud_id'];
-    $stud_name = $_POST['stud_name'];
-    $stud_kulliyah = $_POST['stud_kulliyah'];
-    $stud_country = $_POST['stud_country'];
-    
-    $theQuery = "INSERT INTO students VALUES ('$stud_matric','$stud_name','$stud_kulliyah','$stud_country')";     
-    
-    if($result = $conn->query($theQuery)){
-      $theMessage = "You have successfully added student " . $stud_matric . " whose name is " . $stud_name . ".";
-    }
-    else $theMessage = "There is some problem. Please call IT.";
-  }
+  // Grab values from HTML form
+  $stud_id = $_POST['stud_id'];
+  $stud_name = $_POST['stud_name'];
+  $stud_kulliyah = $_POST['stud_kulliyah'];
+  $stud_country = $_POST['stud_country'];
+
+  // Create the query
+  $theQuery = "INSERT INTO studentszz VALUES ( '$stud_id' , '$stud_name' , '$stud_kulliyah' , '$stud_country' )";
+  
+  // Execute the query and generate error if it fails
+  $result = mysqli_query($conn, $theQuery)
+  or trigger_error("Query Failed! SQL: $theQuery - Error: ". mysqli_error($conn), E_USER_ERROR);
+  
+  // If query succeeded, create 'success' message
+  if ($result)
+    $theMessage = "You have successfully added student " . $stud_id . " whose name is " . $stud_name . ".";  
+} // End of If-Else block
 ?>
 <!doctype html>
 <html lang="en">
@@ -24,7 +33,12 @@
   <title>Add New Student</title>
 </head>
 <body>
-  <?php 
+  <h1>Add a new Student</h1>
+  <!-- Simple menu -->
+  <a href="index.php">View all Students</a>
+  
+  <?php
+    // If the query succeeded, display success message
     if ($theMessage)
       echo $theMessage . "<br>" ;
   ?>
@@ -32,11 +46,11 @@
     <p><label for="stud_id">Student Matric</label><br>
         <input type="text" name="stud_id">
     </p>
-    <p><label for="stud_id">Student Name</label><br>
+    <p><label for="stud_name">Student Name</label><br>
         <input type="text" name="stud_name">
     </p>
-    <p><label for="stud_id">Student Kulliyah</label><br>        
-        <input list="kulliyahs" name="stud_kulliyah"/></label>
+    <p><label for="stud_kulliyah">Student Kulliyah</label><br>        
+        <input list="kulliyahs" name="stud_kulliyah"/>
         <datalist id="kulliyahs">
           <option value="AIKOL">
           <option value="KICT">
@@ -44,7 +58,7 @@
           <option value="ECONS">
         </datalist>
     </p>
-    <p><label for="stud_id">Student Country</label><br>
+    <p><label for="stud_country">Student Country</label><br>
         <input list="countries" name="stud_country"/></label>
         <datalist id="countries">
           <option value="Malaysia">
--- iium-courses-database/htdocs/iiumcourses/index.php
+++ iium-courses-database/htdocs/iiumcourses/index.php
@@ -1,10 +1,31 @@
 <?php
-  require ('connect-db.php');
-  
-  $theQuery = "SELECT * FROM students ORDER BY stud_name";
-  
-  if($result = $conn->query($theQuery)){
-    if ($result->num_rows > 0){
+// The following is required for DB connection
+// Specifically the $conn variable
+require ('connect-db.php');
+
+// Create the query
+$theQuery = "SELECT * FROM students ORDER BY stud_name";
+
+// Execute the query and generate error if it fails
+$result = mysqli_query($conn, $theQuery)
+or trigger_error("Query Failed! SQL: $theQuery - Error: ". mysqli_error($conn), E_USER_ERROR);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>View Students</title>
+</head>
+<body>
+  <h1>All Students</h1>
+  <!-- Simple Menu -->
+  <a href="add-student.php">Add a new Student</a>
+  <br><br>
+  <?php
+  // If $result has values, display table of students
+  if($result) {
       ?>
       <table border="1" cellpadding="10" cellspacing="0">
         <thead>
@@ -16,19 +37,24 @@
           </tr>
         </thead>
         <tbody><?php
-          while ($row = $result->fetch_object()){
-            ?>
-              <tr>
-                <td><?php echo $row->stud_id; ?></td>
-                <td><?php echo $row->stud_name;?></td>
-                <td><?php echo $row->stud_kulliyah;?></td>
-                <td><?php echo $row->stud_country;?></td>            
-              </tr><?php
-          }
-          ?>
+        // Retrieve results and loop through each row by storing it in $row
+        // and calling the associative key related to column values.
+        while($row = mysqli_fetch_assoc($result)) {?>
+          <tr>
+            <td><?php echo $row['stud_id']; ?></td>
+            <td><?php echo $row['stud_name'];?></td>
+            <td><?php echo $row['stud_kulliyah'];?></td>
+            <td><?php echo $row['stud_country'];?></td>            
+          </tr>
+          <?php
+        }?>
         </tbody>
         </table>
       <?php
-    }
+  } // If there are no students, jump to 'else' block.
+  else {
+    echo "There are no student details to display.";
   }
-?>
+  ?>
+</body>
+</html>
